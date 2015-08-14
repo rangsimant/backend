0b7cb4a4fc5fe75625ee034589d34fe9bb323eee
rangsimant <hanwongsa.r@gmail.com>
2015-08-15 02:27:37 +0700

    print connect DB Server

--- Helper/model/Database.php
+++ Helper/model/Database.php
@@ -11,13 +11,21 @@ class Database
 		date_default_timezone_set("Asia/Bangkok");
 		$db_config = parse_ini_file(__DIR__."/../config/database_conf.ini", true);
 		
-		$pdo = new PDO('mysql:host='.$db_config['host'].';
-						dbname='.$db_config['db_name'].';
-						charset=UTF8', 
-						$db_config['user'], 
-						$db_config['pass']);
-
-		$this->fpdo = new FluentPDO($pdo);
-		return $this->fpdo;
+		try 
+		{
+			$pdo = new PDO('mysql:host='.$db_config['host'].';
+							dbname='.$db_config['db_name'].';
+							charset=UTF8', 
+							$db_config['user'], 
+							$db_config['pass']);
+			echo "[DATABASE SERVER] : ".$db_config['host']."\n";
+			$this->fpdo = new FluentPDO($pdo);
+			return $this->fpdo;
+			
+		} catch (Exception $e) 
+		{
+			echo $e->getMessage();
+			exit();
+		}
 	}
 }
